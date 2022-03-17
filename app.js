d4b3a8593185abff72358ac2704faa10a9930f4c
Davi Fernando Rosa <79165566+DaviFernandoRosa@users.noreply.github.com>
2022-03-17 20:22:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var todoRoute = require('./routes/to-do');
+
 var app = express();
 
 app.use(cors());
